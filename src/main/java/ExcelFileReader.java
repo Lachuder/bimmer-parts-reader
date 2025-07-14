0ccc312c223d403e4662e390d294ce9edf839adc
Adrian Debek <adryjano@gmail.com>
2025-07-14 21:54:56 +0200

    added variable and readFile() in ExcelFileReader class

--- src/main/java/ExcelFileReader.java
+++ src/main/java/ExcelFileReader.java
@@ -1,5 +1,51 @@
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
 public class ExcelFileReader {
 
+    private String filePath;
 
+    public ExcelFileReader() {
+    }
+
+    public void readFile(String filePath) {
+
+        try (FileInputStream fis = new FileInputStream(filePath);
+             Workbook workbook = new XSSFWorkbook(fis)) {
+
+            Sheet sheet = workbook.getSheetAt(0); // pierwszy arkusz
+
+            for (Row row : sheet) {
+                for (Cell cell : row) {
+                    switch (cell.getCellType()) {
+                        case STRING:
+                            System.out.print(cell.getStringCellValue() + "\t");
+                            break;
+                        case NUMERIC:
+                            System.out.print(cell.getNumericCellValue() + "\t");
+                            break;
+                        case BOOLEAN:
+                            System.out.print(cell.getBooleanCellValue() + "\t");
+                            break;
+                        case FORMULA:
+                            System.out.print(cell.getCellFormula() + "\t");
+                            break;
+                        default:
+                            System.out.print(" \t");
+                    }
+                }
+                System.out.println(); // nowy wiersz
+            }
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
--- src/main/java/Runner.java
+++ src/main/java/Runner.java
@@ -1,5 +1,14 @@
 public class Runner {
 
+    public static void main(String[] args) {
 
+
+            String filePath = "C:\\Users\\Adriano_Diag\\Downloads\\Oferta specjalna_Listopad.xlsx";
+
+            ExcelFileReader excelFileReader = new ExcelFileReader();
+
+            excelFileReader.readFile(filePath);
+
+    }
 
 }
